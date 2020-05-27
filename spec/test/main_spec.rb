f21b7ea6fb82732a6d551aaa66d76151a46b039d
Katia Xavier <sys1@grupopmz.com.br>
2020-05-27 16:31:08 -0400

    mudança site

---
+++ spec/test/main_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../pages/main_page'
+
+describe 'Access the page' do
+  # before do
+  #     @mainPage.new
+  # end
+
+  it 'Access the Page' do
+    visit 'https://www.trivago.com.br/'
+
+    fill_in 'sQuery', with: 'Manaus'
+
+    find(:xpath, '//li[@id="suggestion-56507/200"]//span[@class="ssg-title"]').click
+
+    find('.js-dealform-button-guests').click
+    find('.roomtype-btn__label', text: 'Individual').click
+
+    click_button 'Pesquisar'
+
+    expect(page).to have_xpath('//span[contains(text(),"Hotel Do Largo")]')
+  end
+end
